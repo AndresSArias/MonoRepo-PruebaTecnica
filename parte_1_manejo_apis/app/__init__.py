a9d685014bcd0e26d9ced40abf6a856665fa5c99
AndresSArias <andres31321@gmail.com>
2024-10-31 18:09:17 -0500

    [Create] redirect for initial rute

--- parte_1_manejo_apis/app/__init__.py
+++ parte_1_manejo_apis/app/__init__.py
@@ -1,19 +1,20 @@
 
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
 from app.api.v1.routes import user, pokemon, capture
-from app.db.session import engine  # Importa el engine desde session.py
-from app.db import base  # Importa base para asegurar que los modelos están listos
+from app.db.session import engine  
+from app.db import base  
 
 # Define la función lifespan para gestionar el ciclo de vida de la app
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+
     # Ejecuta al inicio
     base.SQLModel.metadata.create_all(bind=engine)
-    yield  # Aquí es cuando la app comienza a recibir solicitudes
-    # Código de cierre (se ejecuta al detener la app, si es necesario)
+    yield 
 
-
+#Función inicializadora de la instancia del proyecto FastAPI()
 def create_app():
     
     # Crear la aplicación FastAPI con el parámetro lifespan
@@ -23,5 +24,10 @@ def create_app():
     app.include_router(user.router, prefix="/api/v1")
     app.include_router(pokemon.router, prefix="/api/v1")
     app.include_router(capture.router, prefix="/api/v1")
+
+    #Redirección de la ruta Inicial a la documentación
+    @app.get("/", include_in_schema=False, response_class=RedirectResponse)
+    async def redirect_to_swagger():    
+        return RedirectResponse(url="/docs")    
 
     return app
